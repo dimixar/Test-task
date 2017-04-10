84d0731c0298e4528964b4c57a1c3da14a06051f
Moloșnic Dumitru <dimasalmaz@gmail.com>
2017-04-11 00:08:43 +0300

    Remove unnecessary usings.

--- Assets/Source/Model/PlanetDataContainer.cs
+++ Assets/Source/Model/PlanetDataContainer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace BallTest.Model {
     [CreateAssetMenu(fileName = "new-PlanetsData", menuName = "BallTest/PlanetsData")]
