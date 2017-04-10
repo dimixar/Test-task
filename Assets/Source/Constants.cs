bb7622997d009dc99ba82b22b69f37d28676590f
Moloșnic Dumitru <dimasalmaz@gmail.com>
2017-04-10 23:58:46 +0300

    Add some constants to work with scenes and prefs.

---
+++ Assets/Source/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace BallTest {
+    public class Constants {
+        public class Scenes {
+            public const string menu = "Menu";
+            public const string gameplay = "Gameplay";
+        }
+
+        public class PlayerPrefsKeys {
+            public const string planetName = "planetName";
+            public const string nrOfJumps = "nrOfJumps";
+        }
+    }
+}
