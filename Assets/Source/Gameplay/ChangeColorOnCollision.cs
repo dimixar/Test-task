4b3406032c5a353d2a92a98668189967452b6654
Moloșnic Dumitru <dimasalmaz@gmail.com>
2017-04-11 04:59:52 +0300

    Add random platform coloring when ball collides with it.

---
+++ Assets/Source/Gameplay/ChangeColorOnCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BallTest.Gameplay {
+    public class ChangeColorOnCollision : MonoBehaviour {
+        #region private fields
+        private SpriteRenderer _spriteRenderer;
+        #endregion
+
+        #region Private methods
+        private Color GetRandomColor() {
+            return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+        }
+        #endregion
+
+        #region Monobehaviour methods
+        void Awake() {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            UnityEngine.Assertions.Assert.IsNotNull(_spriteRenderer);
+
+            _spriteRenderer.color = GetRandomColor();
+        }
+
+        void OnCollisionEnter2D(Collision2D coll) {
+            _spriteRenderer.color = GetRandomColor();
+        }
+        #endregion
+    }
+}
