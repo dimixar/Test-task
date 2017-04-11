143f2a8360924727bca5cd562047a221da1534cf
Moloșnic Dumitru <dimasalmaz@gmail.com>
2017-04-11 04:48:14 +0300

    Add directed force on touch.

--- Assets/Source/Gameplay/BallController.cs
+++ Assets/Source/Gameplay/BallController.cs
@@ -13,15 +13,18 @@ namespace BallTest.Gameplay {
 
         #region Private fields
         private Rigidbody2D _rigidBody;
+        private Vector2 _force;
         #endregion
 
         #region Public methods and properties
         public void SetGravity(float gravity) {
             _rigidBody.gravityScale = gravity * -1;
         }
+        #endregion
 
-        public void SetAcceleration(float accel) {
-            _acceleration = accel;
+        #region Private methods
+        private bool IsTouching() {
+            return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary;
         }
         #endregion
 
@@ -30,14 +33,23 @@ namespace BallTest.Gameplay {
             _rigidBody = GetComponent<Rigidbody2D>();
         }
 
+        void FixedUpdate() {
+            if (IsTouching()) {
+                Touch touch = Input.GetTouch(0);
+                Vector2 pos = touch.position;
+                Vector2 dir = ((Vector2)Camera.main.WorldToScreenPoint(transform.localPosition) - pos) * -1;
+
+                _force = (dir.normalized * _acceleration) + _force;
+                _rigidBody.AddForce(_force);
+            }
+            else {
+                _force = Vector2.zero;
+            }
+        }
+
         void OnCollisionEnter2D(Collision2D coll) {
             if (OnBallHit != null)
                 OnBallHit();
-
-            Vector2 force = Vector2.zero;
-            force.y = _acceleration * _rigidBody.mass;
-
-            _rigidBody.AddForce(force);
         }
         #endregion
     }
